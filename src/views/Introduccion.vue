3a6c7dc768313c3969738a42188dcc6fc1a0ce15
Andrea Botello <andrea.botellor@misena.edu.co>
2024-05-23 08:26:16 -0500

    Video Intro

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -9,6 +9,11 @@
           i.fas.fa-info
       h1 Introducción
     
+    figure.mb-5(data-aos="fade-down")
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/zMj9VTyCKIA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Video. Trazo y corte
+    
     p.mb-1(data-aos="fade") La fabricación de ropa incluye numerosas actividades para hacer una prenda, incluyendo procesos como corte, costura y acabado. Aquí conocerá generalidades, tipos y herramientas de trazo y corte.
     p.mb-4(data-aos="fade") La cadena de trazo y corte es uno de los principales procesos de la manufactura textil. Un error implica poner en riesgo la producción generando pérdidas económicas.
 
@@ -30,7 +35,7 @@
       .col-lg-5.col-md-8.col-10.mb-4(data-aos="fade-left")
         img(src='@/assets/curso/temas/intro/intro.svg', alt='')
     
-    .row.justify-content-center(data-aos="fade-down")
+    .row.justify-content-center.mb-4(data-aos="fade-down")
       .col-lg-7.pe-lg-0.mb-4
         .bg-intro.p-4.h-100.d-flex.align-items-center
           img(src='@/assets/curso/temas/intro/img.png', alt='')
@@ -38,7 +43,8 @@
         .bg-22.p-4.h-100
           p Así mismo, se revisará la importancia de contar con un sistema de seguridad y salud en el trabajo en el área de trazo y corte.
           p Además que, los reglamentos internos de seguridad industrial en Colombia son obligatorios, ayudando a ejecutar actividades que tengan como fin proteger la salud del trabajador.
-
+    
+    
 
 
 </template>
